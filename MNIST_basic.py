546c40a3b277fdb3f00346d5ac4c14d34d0ccd12
SwastikUdupa <swastikudupa@gmail.com>
2017-03-14 00:45:06 +0530

    Added comments

--- MNIST_basic.py
+++ MNIST_basic.py
@@ -24,11 +24,12 @@ for i in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-ans = tf.argmax(y,1)
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 # here again the accuracy is determined all over the classes and hence we use the reduce_mean again
 print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
-
-
+''' The sequence of action goes as follows
+    1.accuracy calls correct_prediction
+    2.correct_prediction in turn calls y_
+    Since we already have the value of update w, y is calculated and the comparison is made'''
